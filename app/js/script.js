e9eeead37b2679ae41e5de42e49ff5d80ba4354e
JEMD <x_jemd_x@hotmail.com>
2016-07-03 01:54:48 +0800

    comenzando tab contacto

---
+++ app/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $("#sobre-mi").on("click",function () {
+        $(this).addClass("active");
+        $("#tab-sobre-mi").removeClass("hide");
+        $("#tab-curriculum").addClass("hide");
+        $("#tab-contacto").addClass("hide");
+        $("#curriculum").removeClass("active");
+        $("#contacto").removeClass("active");
+    });
+    $("#curriculum").on("click",function () {
+        $(this).addClass("active");
+        $("#tab-curriculum").removeClass("hide");
+        $("#tab-sobre-mi").addClass("hide");
+        $("#tab-contacto").addClass("hide");
+        $("#sobre-mi").removeClass("active");
+        $("#contacto").removeClass("active");
+    });
+    $("#contacto").on("click",function () {
+        $(this).addClass("active");
+        $("#tab-contacto").removeClass("hide");
+        $("#tab-curriculum").addClass("hide");
+        $("#tab-sobre-mi").addClass("hide");
+        $("#curriculum").removeClass("active");
+        $("#sobre-mi").removeClass("active");
+    });
+    $("img.circle.responsive-img.animated").mouseenter(function () {
+        $(this).addClass("tada");
+        $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass("tada");
+        });
+    });
+});
