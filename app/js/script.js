79a34be6654f5b160b04a5fcf3acd52fee5c4121
JEMD <x_jemd_x@hotmail.com>
2016-07-30 01:59:02 +0800

    colocando info real

--- app/js/script.js
+++ app/js/script.js
@@ -37,5 +37,56 @@ $(document).ready(function() {
             $('#contactForm')[0].reset();
             $(this).addClass("bounceInRight");
         });
-    })
+    });
+    $("#card-image-1").mouseenter(function () {
+       $("#span-card-image-1").removeClass("hide");
+       $("#span-card-image-1").removeClass("bounceOutDown");
+       $("#span-card-image-1").addClass("bounceInUp");
+        $("#span-card-image-1").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass("hide");
+        });
+    });
+    $("#card-image-1").mouseout(function () {
+       $("#span-card-image-1").removeClass("bounceInUp");
+       $("#span-card-image-1").addClass("bounceOutDown");
+       $("#span-card-image-1").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+           $(this).addClass("hide");
+           $(this).removeClass("bounceOutDown");
+       });
+    });
+    $("#card-image-2").mouseenter(function () {
+       $("#span-card-image-2").removeClass("hide");
+       $("#span-card-image-2").removeClass("bounceOutDown");
+       $("#span-card-image-2").addClass("bounceInUp");
+        $("#span-card-image-2").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass("hide");
+        });
+    });
+    $("#card-image-2").mouseout(function () {
+       $("#span-card-image-2").removeClass("bounceInUp");
+       $("#span-card-image-2").addClass("bounceOutDown");
+       $("#span-card-image-2").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+           $(this).addClass("hide");
+           $(this).removeClass("bounceOutDown");
+       });
+    });
+    $("#card-image-3").mouseenter(function () {
+       $("#span-card-image-3").removeClass("hide");
+       $("#span-card-image-3").removeClass("bounceOutDown");
+       $("#span-card-image-3").addClass("bounceInUp");
+        $("#span-card-image-3").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass("hide");
+        });
+    });
+    $("#card-image-3").mouseout(function () {
+       $("#span-card-image-3").removeClass("bounceInUp");
+       $("#span-card-image-3").addClass("bounceOutDown");
+       $("#span-card-image-3").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+           $(this).addClass("hide");
+           $(this).removeClass("bounceOutDown");
+       });
+    });
+    $("#card-image-1").onclick(function () {
+
+    });
 });
