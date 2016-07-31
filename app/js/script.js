244ca4df9f3bdbb36097597ffbdee604ac5e0832
JEMD <x_jemd_x@hotmail.com>
2016-07-31 20:54:19 +0800

    timeline vertical terminado y contacto en busqueda de info adicional

--- app/js/script.js
+++ app/js/script.js
@@ -30,63 +30,90 @@ $(document).ready(function() {
         });
     });
     $("a#send").on("click", function () {
-        $("#card-formulario").removeClass("bounceInRight");
-        $("#card-formulario").addClass("bounceOutRight");
-        $("#card-formulario").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $("#card-formulario")
+            .removeClass("bounceInRight")
+            .addClass("bounceOutRight")
+            .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).removeClass("bounceOutRight");
             $('#contactForm')[0].reset();
             $(this).addClass("bounceInRight");
         });
     });
-    $("#card-image-1").mouseenter(function () {
-       $("#span-card-image-1").removeClass("hide");
-       $("#span-card-image-1").removeClass("bounceOutDown");
-       $("#span-card-image-1").addClass("bounceInUp");
-        $("#span-card-image-1").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-            $(this).removeClass("hide");
+    $("#card-image-1").hover(function () {
+       $("#span-card-image-1")
+           .removeClass("hide")
+           .removeClass("bounceOutDown")
+           .addClass("bounceInUp")
+           .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this)
+                .removeClass("hide");
         });
-    });
-    $("#card-image-1").mouseout(function () {
-       $("#span-card-image-1").removeClass("bounceInUp");
-       $("#span-card-image-1").addClass("bounceOutDown");
-       $("#span-card-image-1").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-           $(this).addClass("hide");
-           $(this).removeClass("bounceOutDown");
+    },
+    function () {
+       $("#span-card-image-1")
+           .removeClass("bounceInUp")
+           .addClass("bounceOutDown")
+           .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+           $(this)
+               .addClass("hide")
+               .removeClass("bounceOutDown");
        });
     });
-    $("#card-image-2").mouseenter(function () {
-       $("#span-card-image-2").removeClass("hide");
-       $("#span-card-image-2").removeClass("bounceOutDown");
-       $("#span-card-image-2").addClass("bounceInUp");
-        $("#span-card-image-2").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-            $(this).removeClass("hide");
+    $("#card-image-2").hover(function () {
+       $("#span-card-image-2")
+           .removeClass("hide")
+           .removeClass("bounceOutDown")
+           .addClass("bounceInUp")
+           .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this)
+                .removeClass("hide");
         });
-    });
-    $("#card-image-2").mouseout(function () {
-       $("#span-card-image-2").removeClass("bounceInUp");
-       $("#span-card-image-2").addClass("bounceOutDown");
-       $("#span-card-image-2").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-           $(this).addClass("hide");
-           $(this).removeClass("bounceOutDown");
+    },
+    function () {
+       $("#span-card-image-2")
+           .removeClass("bounceInUp")
+           .addClass("bounceOutDown")
+           .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+           $(this)
+               .addClass("hide")
+               .removeClass("bounceOutDown");
        });
     });
-    $("#card-image-3").mouseenter(function () {
-       $("#span-card-image-3").removeClass("hide");
-       $("#span-card-image-3").removeClass("bounceOutDown");
-       $("#span-card-image-3").addClass("bounceInUp");
-        $("#span-card-image-3").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-            $(this).removeClass("hide");
+    $("#card-image-3").hover(function () {
+       $("#span-card-image-3")
+           .removeClass("hide")
+           .removeClass("bounceOutDown")
+           .addClass("bounceInUp")
+           .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this)
+                .removeClass("hide");
         });
-    });
-    $("#card-image-3").mouseout(function () {
-       $("#span-card-image-3").removeClass("bounceInUp");
-       $("#span-card-image-3").addClass("bounceOutDown");
-       $("#span-card-image-3").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-           $(this).addClass("hide");
-           $(this).removeClass("bounceOutDown");
+    },
+    function () {
+       $("#span-card-image-3")
+           .removeClass("bounceInUp")
+           .addClass("bounceOutDown")
+           .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+           $(this)
+               .addClass("hide")
+               .removeClass("bounceOutDown");
        });
     });
-    $("#card-image-1").onclick(function () {
 
+    //efecto al pasar el mouse encima por conocimientos
+    var animacion = "flash";
+    $("button").hover(function () {
+        $(this)
+            .addClass("animated")
+            .addClass(animacion)
+    },function () {
+        $("button")
+            .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                $(this)
+                    .removeClass(animacion);
+            });
     });
+
+    /*timeline vertical*/
+    
 });
